da69605e763ad8a891fe1af71dec9e014947b730
mbresnan1701 <bresnan.mw@gmail.com>
2019-06-19 23:45:20 -0700

    added whitelisting for subreddits and a help cmd

--- redditbot.py
+++ redditbot.py
@@ -1,5 +1,6 @@
 import os
 import slack
+import re
 from reddithandler import get_from_reddit
 
 api_token = os.environ['SLACK_BOT_TOKEN']
@@ -7,10 +8,9 @@ api_token = os.environ['SLACK_BOT_TOKEN']
 client = slack.WebClient(token=api_token)
 rtm_client = slack.RTMClient(token=api_token)
 
-subreddit_map = {
-    'hello there': 'prequelmemes',
-    'show me a funny': 'funny'
-}
+whitelisted_subreddits = { 'prequelmemes', 'aww' }
+
+help_message = 'Hi User. Currently available subreddits are: ' + str(whitelisted_subreddits)
 
 @slack.RTMClient.run_on(event='message')
 def say_hello(**payload):
@@ -21,15 +21,18 @@ def say_hello(**payload):
     text = data.get('text')
 
     if text is not None:
-        link = None
+        trigger_text = re.search('^rb\s(\w+)', text)
+        if trigger_text is not None:
+            submitted_command = trigger_text.group(1)
+            print(whitelisted_subreddits)
+            if submitted_command in whitelisted_subreddits:
+                link = get_from_reddit(submitted_command)
 
-        for activator in subreddit_map.keys():
-            if activator in text:
-                link = get_from_reddit(subreddit_map[activator])
-                break
+                if link is not None:
+                    web_client.chat_postMessage(channel=channel_id, text=link)
 
-        if link is not None:
-            web_client.chat_postMessage(channel=channel_id, text=link)
+            elif submitted_command == "help":
+                web_client.chat_postMessage(channel=channel_id, text=help_message)
 
 
 rtm_client.start()
